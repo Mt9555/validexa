8bb9ba96eba8b8c55dd77e5132e82565f22af036
TOPE <topemicheal@yahoo.com>
2023-04-29 20:01:55 -0400

    Update marshmallow config

--- utils/validator.py
+++ utils/validator.py
@@ -5,13 +5,24 @@ def validate_postal_code(postal_code):
   regex = re.compile(r'^\d{5}(?:-\d{4})?$')
   if not regex.match(postal_code):
     raise ValidationError('Invalid postal code format')
+  
+def validate_not_empty(value):
+  if not value:
+    raise ValidationError('Field cannot be empty')
+  
+def validate_addressLine_1(value):
+  if not value:
+    raise ValidationError('Field cannot be empty')
+  if len(value.split()) < 2:
+    raise ValidationError('Invalid addressLine1')
+  # if re.search(r'[^a-zA-Z0-9 ]', value):
+  #   raise ValidationError('Invalid characters in addressLine1')
     
 class AddressSchema(Schema):
-  addressLine1 = fields.Str(required=True)
+  addressLine1 = fields.Str(required=True, validate=[validate_not_empty, validate_addressLine_1])
   addressLine2 = fields.Str(allow_none=True)
-  city = fields.Str(required=True)
-  stateProv = fields.Str(required=True)
+  city = fields.Str(required=True, validate=validate_not_empty)
+  stateProv = fields.Str(required=True, validate=validate_not_empty)
   referenceId = fields.Int(allow_none=True)
   postalCode = fields.Str(required=True, validate=validate_postal_code)
-  country = fields.Str(required=True)
-
+  country = fields.Str(required=True, validate=validate_not_empty)
